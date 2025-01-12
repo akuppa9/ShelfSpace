c70f12e890b29acae3308a2f17e1766ff2e4f846
akuppa9 <amogh.kuppa9@gmail.com>
2025-01-12 17:26:06 -0500

    update and delete endpoints and routes

--- src/controllers/booksController.js
+++ src/controllers/booksController.js
@@ -21,7 +21,7 @@ const createBook = async (req, res) => {
       status,
       rating,
     ]);
-    res.json(result.rows);
+    res.json(result.rows[0]);
   } catch (error) {
     console.error("Error creating book", error);
     res.status(500).json({ error: "Failed to create book" });
@@ -31,37 +31,90 @@ const createBook = async (req, res) => {
 // R - read: GET /api/books
 const getAllBooks = async (req, res) => {
   try {
-    const query = "SELECT * FROM books;";
-    const result = await db.query(query);
+    const user_id = parseInt(req.body.user_id);
+    const bookId = parseInt(req.params.id);
+    const query = "SELECT * FROM books WHERE user_id = $1 AND bookId = $2;";
+    const result = await db.query(query, [user_id, bookId]);
     res.json(result.rows);
   } catch (error) {
     console.error("Error fetching all books from database", error);
-    res.status(500).json({ error: "Failed to fetch books" });
+    res.status(500).json({ error: "Failed to fetch books for particular user" });
   }
 };
 
 // R - read: GET /api/books/:id
 const getBookById = async (req, res) => {
   try {
+    const user_id = parseInt(req.body.user_id);
     const bookId = parseInt(req.params.id);
-    const query = "SELECT * FROM books WHERE id = $1;";
-    const result = await db.query(query, [bookId]);
-    res.json(result.rows);
+    const query = "SELECT * FROM books WHERE user_id = $1 AND id = $2;";
+    const result = await db.query(query, [user_id, bookId]);
+    res.json(result.rows[0]);
   } catch (error) {
     console.error("Error fetching book by id from database", error);
-    res.status(500).json({ error: "Failed to fetch book by id" });
+    res.status(500).json({ error: "Failed to fetch book by id for particular user" });
   }
 };
 
-const deleteBookById = async(req, res) => {
+// U - update: PUT /api/books/:id
+const updateBook = async (req, res) => {
   try {
+    const { user_id, title, author, status, rating } = req.body;
     const bookId = parseInt(req.params.id);
-    //logic to delete 
+    const query = "SELECT * FROM books WHERE user_id = $1 AND id = $2;";
+    const result = await db.query(query, [user_id, bookId]);
+    const book = result.rows[0];
+
+    const updateBook = {
+      user_id: user_id || book.user_id,
+      title: title || book.title,
+      author: author || book.author,
+      status: status || book.status,
+      rating: rating || book.rating,
+    };
+
+    const updateQuery = `UPDATE books SET 
+      user_id = $1, 
+      title = $2, 
+      author = $3, 
+      status = $4, 
+      rating = $5
+      WHERE id = $6
+      RETURNING *;
+      `;
+    const updateResult = await db.query(updateQuery, [
+      updateBook.user_id,
+      updateBook.title,
+      updateBook.author,
+      updateBook.status,
+      updateBook.rating,
+      bookId,
+    ]);
+    res.json(updateResult.rows[0]);
+  } catch (error) {
+    console.error("Error updating book by id", error);
+    res.status(500).json({ error: "Failed to update book by id" });
   }
-}
+};
+
+const deleteBook = async (req, res) => {
+  try {
+    const user_id = parseInt(req.body.user_id);
+    const bookId = parseInt(req.params.id);
+    const query = `DELETE FROM books WHERE user_id = $1 AND id = $2
+    RETURNING *;`;
+    const result = await db.query(query, [user_id, bookId]);
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error("Error deleting book by id from database", error);
+    res.status(500).json({ error: "Failed to delete book by id" });
+  }
+};
 
 module.exports = {
   createBook,
   getAllBooks,
   getBookById,
+  updateBook,
+  deleteBook,
 };
--- src/routes/books.js
+++ src/routes/books.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const router = express.Router();
-const { createBook, getAllBooks, getBookById } = require("../controllers/booksController");
+const { createBook, getAllBooks, getBookById, updateBook, deleteBook } = require("../controllers/booksController");
 
 // Route for POST api/books
 router.post('/', createBook);
@@ -12,5 +12,11 @@ router.get('/', getAllBooks);
 
 // Route for GET api/books/:id
 router.get('/:id', getBookById);
+
+// Route for PUT api/books/:id
+router.put('/:id', updateBook);
+
+// Route for DELETE api/books/:id
+router.delete('/:id', deleteBook);
 
 module.exports = router;
