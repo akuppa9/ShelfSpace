e3fcfeb88768b0e5fba6a32f44ba120d56845cde
akuppa9 <amogh.kuppa9@gmail.com>
2025-02-09 16:52:23 -0500

    display username on library page

--- client/src/components/AccountState.jsx
+++ client/src/components/AccountState.jsx
@@ -1,19 +1,28 @@
 import React from "react";
 
+const displayUserName = () => {
+  const username = localStorage.getItem("username");
+  return username ? `${username}'s Space` : "Guest's Space"
+}
 const AccountState = ({ onLogout, onDeleteAccount }) => (
-  <div className="flex justify-end items-center mb-6 space-x-4">
-    <button
-      onClick={onLogout}
-      className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600"
-    >
-      Logout
-    </button>
-    <button
-      onClick={onDeleteAccount}
-      className="bg-red-700 text-white px-4 py-2 rounded-lg hover:bg-red-800"
-    >
-      Delete Account
-    </button>
+  <div className="flex justify-between items-center p-4">
+    <div className="text-gray-400 text-5xl font-semibold">
+      {displayUserName()}
+    </div>
+    <div className="flex justify-end items-center mb-6 space-x-4">
+      <button
+        onClick={onLogout}
+        className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600"
+      >
+        Logout
+      </button>
+      <button
+        onClick={onDeleteAccount}
+        className="bg-red-700 text-white px-4 py-2 rounded-lg hover:bg-red-800"
+      >
+        Delete Account
+      </button>
+    </div>
   </div>
 );
 
